843bf35f5bbe633201de03a9ba9cda83e6d40b02
Seja19 <144514108+Seja19@users.noreply.github.com>
2023-11-08 15:40:46 +0000

    container config done

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=144231;
+let accountEmail="seja19@gmail.com";
+var accountPassword="4321";
+
+// accountId=2;
+console.log(accountId);
+accountEmail="123@";
+accountPassword="1234";
+
+console.log([accountId, accountEmail, accountPassword]);
+console.table([accountId, accountEmail, accountPassword]);
