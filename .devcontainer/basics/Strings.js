7269533f7f4a4a8ff3fc4b78122c650fc29529b3
Seja19 <144514108+Seja19@users.noreply.github.com>
2023-11-15 05:08:44 +0000

    nums and maths

---
+++ .devcontainer/basics/Strings.js
@@ -0,0 +1,13 @@
+const game='Seja'
+console.log(game)
+const game1=new String('Sejal')
+console.log(game1)
+console.log(game1.charAt(2))
+console.log(game1.length)
+console.log(game1.toUpperCase());
+console.log(game1.indexOf('a'));
+console.log(game1.substring(2,4))
+const myString=game1.substring(1,4);
+console.log(myString);
+const newString="     madhur   "
+console.log(newStr);
---
+++ .devcontainer/basics/nums_maths.js
@@ -0,0 +1,38 @@
+// NUMBERS
+
+/*const num=400
+console.log(num);
+console.log(typeof(num));
+
+const num1=new Number(4000)
+console.log(num1);
+console.log(typeof(num1));
+
+console.log(num1.toString().length);
+console.log(num1.toFixed(1));
+
+const otherNum=123.89734
+console.log(otherNum.toPrecision(4));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));*/
+
+// MATHS
+
+/*console.log(Math);
+console.log(Math.abs(-3)); // only negative to positive
+console.log(Math.round(9.8));
+console.log(Math.ceil(6.1))// ciel is used to round a number to it's greater number  
+console.log(Math.floor(7.9));
+console.log(Math.sqrt(25));
+console.log(Math.pow(3,2));
+console.log(Math.min(3,4,5,6));
+console.log(Math.max(7,8,9));
+console.log(Math.random());
+console.log((Math.random()*3)+1);*/
+
+const min=10
+const max=20
+
+console.log(Math.round(Math.random()*(max-min+1))+min);
+
